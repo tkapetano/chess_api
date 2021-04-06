39da22f3ebc318b812f685f19b00557b16373a07
tobi <tkapetano90@googlemail.com>
2021-04-06 21:25:47 +0200

    added default sql alchemy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fastapi
 import uvicorn
 
 from api import chess_api
+from sql_app import sql_access
 from views import home
 
 api = fastapi.FastAPI()
@@ -10,6 +11,7 @@ api = fastapi.FastAPI()
 def configure():
     api.include_router(home.router)
     api.include_router(chess_api.router)
+    api.include_router(sql_access.router)
 
 
 configure()
